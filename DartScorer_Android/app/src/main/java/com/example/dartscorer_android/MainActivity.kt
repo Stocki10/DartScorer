51ba4e2ef4ba3f2c7eb34940572442ed0775a0d0
LS <l.stockmann@gmail.com>
2026-02-20 17:19:23 +0100

    FIx Subtraction

--- DartScorer_Android/app/src/main/java/com/example/dartscorer_android/MainActivity.kt
+++ DartScorer_Android/app/src/main/java/com/example/dartscorer_android/MainActivity.kt
@@ -5,12 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.dartscorer_android.ui.DartsGameScreen
 import com.example.dartscorer_android.ui.theme.DartScorer_AndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DartScorer_AndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    DartsGameScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DartScorer_AndroidTheme {
-        Greeting("Android")
     }
 }
